0e11f3e72b81d314f8dc9ea83e38d846954ba5cd
Ekapop <eploentham@gmail.com>
2019-05-28 17:18:07 +0700

    1
    1

--- modernpos_pos/gui/Form3.Designer.cs
+++ modernpos_pos/gui/Form3.Designer.cs
@@ -41,11 +41,16 @@
             this.txtTopUp2 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtTopUp3 = new System.Windows.Forms.TextBox();
+            this.c1DockingTab1 = new C1.Win.C1Command.C1DockingTab();
+            this.c1DockingTabPage1 = new C1.Win.C1Command.C1DockingTabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).BeginInit();
+            this.c1DockingTab1.SuspendLayout();
+            this.c1DockingTabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(255, 113);
+            this.btnPrint.Location = new System.Drawing.Point(88, 89);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(75, 23);
             this.btnPrint.TabIndex = 8;
@@ -56,7 +61,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(188, 58);
+            this.label1.Location = new System.Drawing.Point(21, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 7;
@@ -65,14 +70,14 @@
             // cboPrinter
             // 
             this.cboPrinter.FormattingEnabled = true;
-            this.cboPrinter.Location = new System.Drawing.Point(255, 55);
+            this.cboPrinter.Location = new System.Drawing.Point(88, 31);
             this.cboPrinter.Name = "cboPrinter";
             this.cboPrinter.Size = new System.Drawing.Size(272, 21);
             this.cboPrinter.TabIndex = 6;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(255, 153);
+            this.textBox1.Location = new System.Drawing.Point(88, 129);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(262, 20);
             this.textBox1.TabIndex = 9;
@@ -80,7 +85,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 156);
+            this.label2.Location = new System.Drawing.Point(21, 132);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 10;
@@ -89,7 +94,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(188, 300);
+            this.label3.Location = new System.Drawing.Point(21, 276);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(24, 13);
             this.label3.TabIndex = 12;
@@ -97,7 +102,7 @@
             // 
             // txtDesk
             // 
-            this.txtDesk.Location = new System.Drawing.Point(255, 297);
+            this.txtDesk.Location = new System.Drawing.Point(88, 273);
             this.txtDesk.Name = "txtDesk";
             this.txtDesk.Size = new System.Drawing.Size(262, 20);
             this.txtDesk.TabIndex = 11;
@@ -105,7 +110,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(188, 182);
+            this.label4.Location = new System.Drawing.Point(21, 158);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(46, 13);
             this.label4.TabIndex = 14;
@@ -113,7 +118,7 @@
             // 
             // txtTopUp1
             // 
-            this.txtTopUp1.Location = new System.Drawing.Point(255, 179);
+            this.txtTopUp1.Location = new System.Drawing.Point(88, 155);
             this.txtTopUp1.Name = "txtTopUp1";
             this.txtTopUp1.Size = new System.Drawing.Size(262, 20);
             this.txtTopUp1.TabIndex = 13;
@@ -121,7 +126,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(188, 208);
+            this.label5.Location = new System.Drawing.Point(21, 184);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(46, 13);
             this.label5.TabIndex = 16;
@@ -129,7 +134,7 @@
             // 
             // txtTopUp2
             // 
-            this.txtTopUp2.Location = new System.Drawing.Point(255, 205);
+            this.txtTopUp2.Location = new System.Drawing.Point(88, 181);
             this.txtTopUp2.Name = "txtTopUp2";
             this.txtTopUp2.Size = new System.Drawing.Size(262, 20);
             this.txtTopUp2.TabIndex = 15;
@@ -137,7 +142,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(188, 234);
+            this.label6.Location = new System.Drawing.Point(21, 210);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(46, 13);
             this.label6.TabIndex = 18;
@@ -145,35 +150,57 @@
             // 
             // txtTopUp3
             // 
-            this.txtTopUp3.Location = new System.Drawing.Point(255, 231);
+            this.txtTopUp3.Location = new System.Drawing.Point(88, 207);
             this.txtTopUp3.Name = "txtTopUp3";
             this.txtTopUp3.Size = new System.Drawing.Size(262, 20);
             this.txtTopUp3.TabIndex = 17;
+            // 
+            // c1DockingTab1
+            // 
+            this.c1DockingTab1.Controls.Add(this.c1DockingTabPage1);
+            this.c1DockingTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1DockingTab1.Location = new System.Drawing.Point(0, 0);
+            this.c1DockingTab1.Name = "c1DockingTab1";
+            this.c1DockingTab1.Size = new System.Drawing.Size(800, 450);
+            this.c1DockingTab1.TabIndex = 19;
+            this.c1DockingTab1.TabsSpacing = 5;
+            // 
+            // c1DockingTabPage1
+            // 
+            this.c1DockingTabPage1.Controls.Add(this.label1);
+            this.c1DockingTabPage1.Controls.Add(this.label6);
+            this.c1DockingTabPage1.Controls.Add(this.cboPrinter);
+            this.c1DockingTabPage1.Controls.Add(this.txtTopUp3);
+            this.c1DockingTabPage1.Controls.Add(this.btnPrint);
+            this.c1DockingTabPage1.Controls.Add(this.label5);
+            this.c1DockingTabPage1.Controls.Add(this.textBox1);
+            this.c1DockingTabPage1.Controls.Add(this.txtTopUp2);
+            this.c1DockingTabPage1.Controls.Add(this.label2);
+            this.c1DockingTabPage1.Controls.Add(this.label4);
+            this.c1DockingTabPage1.Controls.Add(this.txtDesk);
+            this.c1DockingTabPage1.Controls.Add(this.txtTopUp1);
+            this.c1DockingTabPage1.Controls.Add(this.label3);
+            this.c1DockingTabPage1.Location = new System.Drawing.Point(1, 24);
+            this.c1DockingTabPage1.Name = "c1DockingTabPage1";
+            this.c1DockingTabPage1.Size = new System.Drawing.Size(798, 425);
+            this.c1DockingTabPage1.TabIndex = 0;
+            this.c1DockingTabPage1.Text = "Page1";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.txtTopUp3);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtTopUp2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtTopUp1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtDesk);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnPrint);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cboPrinter);
+            this.Controls.Add(this.c1DockingTab1);
             this.Name = "Form3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.c1DockingTab1)).EndInit();
+            this.c1DockingTab1.ResumeLayout(false);
+            this.c1DockingTabPage1.ResumeLayout(false);
+            this.c1DockingTabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -192,5 +219,7 @@
         private System.Windows.Forms.TextBox txtTopUp2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtTopUp3;
+        private C1.Win.C1Command.C1DockingTab c1DockingTab1;
+        private C1.Win.C1Command.C1DockingTabPage c1DockingTabPage1;
     }
 }
