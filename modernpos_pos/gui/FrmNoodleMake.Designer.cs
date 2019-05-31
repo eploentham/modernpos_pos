b235ec2310901e6ef81889297b1cf7ec367423e1
Ekapop <eploentham@gmail.com>
2019-05-31 07:05:18 +0700

    1
    1

---
+++ modernpos_pos/gui/FrmNoodleMake.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace modernpos_pos.gui
+{
+    partial class FrmNoodleMake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnName = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.c1SuperLabel1 = new C1.Win.C1SuperTooltip.C1SuperLabel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pnQty = new System.Windows.Forms.Panel();
+            this.c1SuperLabel2 = new C1.Win.C1SuperTooltip.C1SuperLabel();
+            this.c1PictureBox1 = new C1.Win.C1Input.C1PictureBox();
+            this.c1PictureBox2 = new C1.Win.C1Input.C1PictureBox();
+            this.c1SuperLabel3 = new C1.Win.C1SuperTooltip.C1SuperLabel();
+            this.panel1.SuspendLayout();
+            this.pnName.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.pnQty.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pnName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1144, 785);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnName
+            // 
+            this.pnName.Controls.Add(this.pnQty);
+            this.pnName.Controls.Add(this.panel7);
+            this.pnName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnName.Location = new System.Drawing.Point(0, 0);
+            this.pnName.Name = "pnName";
+            this.pnName.Size = new System.Drawing.Size(1144, 62);
+            this.pnName.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(571, 723);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(571, 62);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(573, 723);
+            this.panel3.TabIndex = 1;
+            // 
+            // c1SuperLabel1
+            // 
+            this.c1SuperLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1SuperLabel1.Location = new System.Drawing.Point(0, 0);
+            this.c1SuperLabel1.Name = "c1SuperLabel1";
+            this.c1SuperLabel1.Size = new System.Drawing.Size(920, 62);
+            this.c1SuperLabel1.TabIndex = 0;
+            this.c1SuperLabel1.Text = "c1SuperLabel1";
+            this.c1SuperLabel1.UseMnemonic = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(571, 242);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 242);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(571, 275);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(0, 517);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(571, 206);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.c1SuperLabel1);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(920, 62);
+            this.panel7.TabIndex = 0;
+            // 
+            // pnQty
+            // 
+            this.pnQty.Controls.Add(this.c1SuperLabel3);
+            this.pnQty.Controls.Add(this.c1PictureBox2);
+            this.pnQty.Controls.Add(this.c1PictureBox1);
+            this.pnQty.Controls.Add(this.c1SuperLabel2);
+            this.pnQty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnQty.Location = new System.Drawing.Point(920, 0);
+            this.pnQty.Name = "pnQty";
+            this.pnQty.Size = new System.Drawing.Size(224, 62);
+            this.pnQty.TabIndex = 1;
+            // 
+            // c1SuperLabel2
+            // 
+            this.c1SuperLabel2.Location = new System.Drawing.Point(73, 3);
+            this.c1SuperLabel2.Name = "c1SuperLabel2";
+            this.c1SuperLabel2.Size = new System.Drawing.Size(75, 23);
+            this.c1SuperLabel2.TabIndex = 0;
+            this.c1SuperLabel2.Text = "c1SuperLabel2";
+            this.c1SuperLabel2.UseMnemonic = true;
+            // 
+            // c1PictureBox1
+            // 
+            this.c1PictureBox1.Location = new System.Drawing.Point(10, 25);
+            this.c1PictureBox1.Name = "c1PictureBox1";
+            this.c1PictureBox1.Size = new System.Drawing.Size(43, 31);
+            this.c1PictureBox1.TabIndex = 1;
+            this.c1PictureBox1.TabStop = false;
+            // 
+            // c1PictureBox2
+            // 
+            this.c1PictureBox2.Location = new System.Drawing.Point(171, 25);
+            this.c1PictureBox2.Name = "c1PictureBox2";
+            this.c1PictureBox2.Size = new System.Drawing.Size(43, 31);
+            this.c1PictureBox2.TabIndex = 2;
+            this.c1PictureBox2.TabStop = false;
+            // 
+            // c1SuperLabel3
+            // 
+            this.c1SuperLabel3.Location = new System.Drawing.Point(73, 25);
+            this.c1SuperLabel3.Name = "c1SuperLabel3";
+            this.c1SuperLabel3.Size = new System.Drawing.Size(75, 30);
+            this.c1SuperLabel3.TabIndex = 3;
+            this.c1SuperLabel3.Text = "c1SuperLabel3";
+            this.c1SuperLabel3.UseMnemonic = true;
+            // 
+            // FrmNoodleMake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 785);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmNoodleMake";
+            this.Text = "FrmNoodleMake";
+            this.panel1.ResumeLayout(false);
+            this.pnName.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.pnQty.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.c1PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnName;
+        private C1.Win.C1SuperTooltip.C1SuperLabel c1SuperLabel1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel pnQty;
+        private System.Windows.Forms.Panel panel7;
+        private C1.Win.C1SuperTooltip.C1SuperLabel c1SuperLabel2;
+        private C1.Win.C1SuperTooltip.C1SuperLabel c1SuperLabel3;
+        private C1.Win.C1Input.C1PictureBox c1PictureBox2;
+        private C1.Win.C1Input.C1PictureBox c1PictureBox1;
+    }
+}
