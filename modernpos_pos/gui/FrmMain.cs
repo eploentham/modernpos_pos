0fb940f1ea7120bf26379ddb6600f47006c56f4e
Ekapop <eploentham@gmail.com>
2020-01-27 08:58:18 +0700

    Update FrmMain.cs

--- modernpos_pos/gui/FrmMain.cs
+++ modernpos_pos/gui/FrmMain.cs
@@ -128,7 +128,7 @@ namespace modernpos_pos.gui
             splash.Dispose();
             String date = "";
             date = DateTime.Now.Year + "-" + DateTime.Now.ToString("MM-dd");
-            this.Text = "Start 2019-04-17 Last Update 2020-01-21 format date " + date;
+            this.Text = "Start 2019-04-17 Last Update 2020-01-26 format date " + date;
             txtHeader.Text = mposC.txtHeader;
             if (mposC.iniC.statusAppToGo.Equals("1"))
             {
